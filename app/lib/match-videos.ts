aa6f351d8149285bf0af9735d0f736a5ddac2dca
mrsa-northam <mrsa.northam@gmail.com>
2026-08-08 20:55:18 -0500

    Add more match video links

--- app/lib/match-videos.ts
+++ app/lib/match-videos.ts
@@ -3,7 +3,10 @@ export const swingVisionMatchLinks: Record<string, string> = {
   "D1-008": "https://swing.vision/matches/sw2-tsVu6B0",
   "D1-009": "https://swing.vision/matches/sw2-g4xRsZM",
   "D1-010": "https://swing.vision/matches/sw2-y0TEJP8",
-  "D1-025": "https://swing.vision/matches/sw2-xhvzUEg"
+  "D1-019": "https://swing.vision/matches/sw2-tXs3My4",
+  "D1-025": "https://swing.vision/matches/sw2-xhvzUEg",
+  "D1-043": "https://swing.vision/matches/sw2-U7xNNac",
+  "D1-044": "https://swing.vision/matches/sw2-VFjwGA8"
 };
 
 export function getSwingVisionMatchUrl(publicMatchId: string) {
